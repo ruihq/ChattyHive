0b9315e4215187e2ea9327ba7d9fbd8d8f2b7d56
Lumine <120704613+ruihq@users.noreply.github.com>
2023-02-17 19:09:33 +0800

    Update index.js

--- chattyhive-web/src/index.js
+++ chattyhive-web/src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ChattyHive from './ChattyHive';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
